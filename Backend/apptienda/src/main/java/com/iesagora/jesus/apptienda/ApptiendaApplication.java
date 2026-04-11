f08dbda36a4361651c3e21ad76f8dd7a1e9c0fe2
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-11 14:51:07 +0200

    Commit inicial de backend estructura y dependencias iniciales

---
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/ApptiendaApplication.java
@@ -0,0 +1,13 @@
+package com.iesagora.jesus.apptienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptiendaApplication.class, args);
+	}
+
+}
