5ef0238933d8638f3c10a838aec5f8d5de02cf67
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-21 18:47:15 +0200

    Security Config

--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/security/SecurityConfig.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/security/SecurityConfig.java
@@ -1,16 +1,66 @@
 package com.iesagora.jesus.apptienda.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+
+
+import com.iesagora.jesus.apptienda.business.repositories.UsuarioRepository;
 
 @Configuration
 public class SecurityConfig {
 	
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
-
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+	    return config.getAuthenticationManager();
+	}
+	
+	@Bean
+	public UserDetailsService userDetailsService(UsuarioRepository usuarioRepository) {
+	    return email -> usuarioRepository.findByEmail(email);
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
+		
+		http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated())
+        		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        		.authenticationProvider(authenticationProvider)
+        		.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService,
+	                                                     PasswordEncoder passwordEncoder) {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+	    authProvider.setUserDetailsService(userDetailsService);
+	    authProvider.setPasswordEncoder(passwordEncoder);
+	    return authProvider;
+	}
+	
 }
