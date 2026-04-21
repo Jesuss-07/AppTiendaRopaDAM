78bb81294c5a2c440c55a459673ba7cf9d8ba736
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-21 19:21:39 +0200

    Se añadieron las validaaciones de Email y NotBlank al email y a password

--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/dto/UsuarioDTO.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/dto/UsuarioDTO.java
@@ -1,5 +1,7 @@
 package com.iesagora.jesus.apptienda.business.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +15,11 @@ import lombok.ToString;
 @ToString
 public class UsuarioDTO {
 	
+	@Email(message = "Formato de email erroneo")
+	@NotBlank(message = "El email es obligatorio")
 	private String email;
+	
+	@NotBlank(message = "La contraseña es obligatoria")
 	private String password;
 
 }
