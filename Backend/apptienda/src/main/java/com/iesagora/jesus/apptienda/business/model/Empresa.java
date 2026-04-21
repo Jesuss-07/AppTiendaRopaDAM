b7f3c33d6fdbd3aea153d47edbd4f2ec6c863252
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-21 19:22:14 +0200

    Se agrego la clase Empresa y se relleno la clase Vendedor

---
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/model/Empresa.java
@@ -0,0 +1,48 @@
+package com.iesagora.jesus.apptienda.business.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "Empresa")
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_empresa")
+	private Long idEmpresa;
+	
+	@Column(name = "nombre_empresa")
+	private String nombreEmpresa;
+	
+	@Column(name = "cif", unique = true)
+	private String cif;
+	
+	@Column(name = "email_contacto")
+	private String emailContacto;
+	
+	@Column(name = "telefono_contacto")
+	private String telefonoContacto;
+
+	@Column(name = "direccion_sede")
+	private String direccionSede;
+	
+	@Column(name = "logo_empresa")
+	private String logoEmpresa;
+
+
+}
--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/model/Vendedor.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/model/Vendedor.java
@@ -1,6 +1,9 @@
 package com.iesagora.jesus.apptienda.business.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 
@@ -13,5 +16,12 @@ public class Vendedor extends Usuario{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	@ManyToOne
+	@JoinColumn(name = "id_empresa")
+	private Empresa empresa;
+	
+	@Column(name = "numero_empleado")
+	private String numeroEmpleado;
+	
 }
