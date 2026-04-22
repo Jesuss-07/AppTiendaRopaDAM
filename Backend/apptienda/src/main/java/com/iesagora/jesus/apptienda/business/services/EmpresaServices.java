975b73af10d04b456008c852de8fdbbb5a4fb9b1
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-22 17:14:15 +0200

    Metodo de registro de la empresa terminado no probado

--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/services/EmpresaServices.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/services/EmpresaServices.java
@@ -1,5 +1,9 @@
 package com.iesagora.jesus.apptienda.business.services;
 
+import com.iesagora.jesus.apptienda.business.dto.RegistroEmpresaDTO;
+
 public interface EmpresaServices {
 
+	void registroEmpresa(RegistroEmpresaDTO empresaDTO);
+	
 }
--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/services/impl/EmpresaServicesImpl.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/services/impl/EmpresaServicesImpl.java
@@ -1,7 +1,41 @@
 package com.iesagora.jesus.apptienda.business.services.impl;
 
+import com.iesagora.jesus.apptienda.business.dto.RegistroEmpresaDTO;
+import com.iesagora.jesus.apptienda.business.model.Empresa;
+import com.iesagora.jesus.apptienda.business.repositories.EmpresaRepository;
 import com.iesagora.jesus.apptienda.business.services.EmpresaServices;
 
 public class EmpresaServicesImpl implements EmpresaServices{
+	
+	private final EmpresaRepository empresaRepository;
+	
+	public EmpresaServicesImpl(EmpresaRepository empresaRepository) {
+		this.empresaRepository = empresaRepository;
+	}
+
+	@Override
+	public void registroEmpresa(RegistroEmpresaDTO empresaDTO) {
+
+		if(empresaRepository.existsByCif(empresaDTO.getCif()))
+			throw new IllegalStateException("Ya existe una empresa con este CIF");
+		
+		Empresa empresa = crearEmpresa(empresaDTO);
+		empresaRepository.save(empresa);
+		
+	}
+	
+	private Empresa crearEmpresa(RegistroEmpresaDTO empresaDTO) {
+		
+		Empresa empresa = new Empresa();
+		
+		empresa.setCif(empresaDTO.getCif());
+		empresa.setDireccionSede(empresaDTO.getDireccionSede());
+		empresa.setEmailContacto(empresaDTO.getEmailContacto());
+		empresa.setLogoEmpresa(empresaDTO.getLogoEmpresa());
+		empresa.setNombreEmpresa(empresaDTO.getNombreEmpresa());
+		empresa.setTelefonoContacto(empresaDTO.getTelefonoContacto());
+		
+		return empresa;
+	}
 
 }
