d6c7571f4c328acc4296b0c37eaaa3055b4e3611
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-22 18:03:10 +0200

    Se agrego el controlador de la empresa registro de esta misma comprobado y terminado

---
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/presentation/controllers/EmpresaController.java
@@ -0,0 +1,34 @@
+package com.iesagora.jesus.apptienda.presentation.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iesagora.jesus.apptienda.business.dto.RegistroEmpresaDTO;
+import com.iesagora.jesus.apptienda.business.services.EmpresaServices;
+
+@RestController
+@RequestMapping("/empresa")
+public class EmpresaController {
+	
+	private EmpresaServices empresaServices;
+	
+	public EmpresaController(EmpresaServices empresaServices) {
+		this.empresaServices = empresaServices;
+	}
+	
+	@PostMapping("/registro")
+	public ResponseEntity<?> registroEmpresa(@RequestBody RegistroEmpresaDTO empresaDTO){
+		
+		try {
+			empresaServices.registroEmpresa(empresaDTO);
+			return ResponseEntity.status(201).build();
+		}catch(Exception e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+	}
+
+}
