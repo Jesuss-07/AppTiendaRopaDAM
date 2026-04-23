04a8bc7253ff9027ef4c4f402f1960a490a54582
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-24 01:03:34 +0200

    Servicio auth

---
+++ Frontend/apptienda/src/app/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { reportUnhandledError } from "rxjs/internal/util/reportUnhandledError";
+import { jwtDecode } from "jwt-decode";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthService {
+    private apiUrl = 'http://localhost:8088/auth';
+
+    constructor(private http: HttpClient) {}
+
+    login(usuario: any) {
+        return this.http.post(`${this.apiUrl}/login`, usuario);
+    }
+
+    registroCliente(cliente: any) {
+        return this.http.post(`${this.apiUrl}/registro/cliente`, cliente);
+    }
+
+    registroVendedor(vendedor: any) {
+        return this.http.post(`${this.apiUrl}/registro/vendedor`, vendedor);
+    }
+
+    guardarToken(token: string) {
+        localStorage.setItem('token', token);
+    }
+
+    obtenerToken() {
+        return localStorage.getItem('token');
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+    }
+
+    estadoAunteticado() {
+        return this.obtenerToken() != null;
+    }
+
+    getRol(): string | null {
+        const token = this.obtenerToken();
+        if (!token) return null;
+
+        const datos: any = jwtDecode(token);
+        return datos.rol;
+    }
+}
