93b3335d84338b5b9a70e0649c034e02e2fc1b66
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-24 01:06:10 +0200

    Rutas

--- Frontend/apptienda/src/app/app.routes.ts
+++ Frontend/apptienda/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { ClienteComponent } from './pages/registrar/cliente/cliente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'registro/cliente', component: ClienteComponent },
+    { path: '', redirectTo: 'login' , pathMatch: 'full' }
+];
