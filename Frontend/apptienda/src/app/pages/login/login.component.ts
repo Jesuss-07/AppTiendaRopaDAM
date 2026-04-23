0994c0d085515f39ff7c58664be7a8d9d4d54b0d
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-04-24 01:04:35 +0200

    Componentes de login ts html y css

---
+++ Frontend/apptienda/src/app/pages/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
+  standalone: true,
+  imports: [FormsModule]
+})
+export class LoginComponent {
+
+  email: string = "";
+  password: string = "";
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onLogin() {
+
+    const usuario = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.login(usuario).subscribe({
+      next: (res: any) => {
+
+        console.log("Login correcto", res);
+
+        this.authService.guardarToken(res.token);
+
+        const rol = this.authService.getRol();
+        console.log("Rol:", rol);
+
+        if (rol === "CLIENTE") {
+          this.router.navigate(['/cliente']);
+        } else if (rol === "VENDEDOR") {
+          this.router.navigate(['/empresa']);
+        } else {
+          this.router.navigate(['/']);
+        }
+      },
+
+      error: (err) => {
+        console.log("Error en login", err);
+      }
+    });
+  }
+}
