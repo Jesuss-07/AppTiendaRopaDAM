c1b378a3361e6f5824f5c213aca800e18b369c47
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-21 18:29:23 +0200

    Conexion de rutas entre registro y registro cliente/vendedor

--- Frontend/apptienda/src/app/pages/registrar/eleccion/eleccion.component.ts
+++ Frontend/apptienda/src/app/pages/registrar/eleccion/eleccion.component.ts
@@ -1,22 +1,14 @@
 import { Component } from "@angular/core";
-import { Route, Router } from "@angular/router";
+import { Router } from "@angular/router";
+import { RouterModule } from "@angular/router";
 
 @Component({
     selector: "app-eleccion-registro",
     templateUrl: "./eleccion.component.html",
     styleUrls: ["./eleccion.component.css"],
     standalone: true,
-    imports: []
+    imports: [
+        RouterModule
+    ]
 })
-export class EleccionComponent {
-    constructor(private router: Router) {}
-
-    registrarCliente() {
-        this.router.navigate(['/registro/cliente']);
-    }
-
-    registrarVendedor() {
-        this.router.navigate(['/registro/vendedor']);
-    }
-
-}
+export class EleccionComponent {}
