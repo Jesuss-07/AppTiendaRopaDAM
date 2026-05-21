a052bd01244d63db5f3f802c1c3a4127f42adab0
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-21 19:17:28 +0200

    Se actualizo el estilo de registro/cliente

--- Frontend/apptienda/src/app/pages/registrar/cliente/cliente.component.ts
+++ Frontend/apptienda/src/app/pages/registrar/cliente/cliente.component.ts
@@ -30,6 +30,12 @@ export class ClienteComponent {
     constructor(private authService: AuthService, private router: Router) {}
 
     onRegistroCliente() {
+
+        if(!this.validarPassword(this.cliente.password)) {
+            alert("La contraseña no cumple los requisitos:\n- 8 caracteres\n- Mayúscula y minúscula\n- Número\n- Carácter especial");
+            return;
+        }
+
         this.authService.registroCliente(this.cliente).subscribe({
             next: (res) => {
                 console.log('Registro exitoso:', res);
@@ -39,5 +45,35 @@ export class ClienteComponent {
                 console.error('Error en el registro:', err);
             }
         });
+    }
+
+    validarPassword(password: string = ''): boolean {
+        const minimoOcho = password.length >= 8;
+        const tieneNumero = /[0-9]/.test(password);
+        const tieneLetraMayuscula = /[A-Z]/.test(password);
+        const tieneLetraMinuscula = /[a-z]/.test(password);
+        const tieneCaracterEspecial = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+        return minimoOcho && tieneNumero && tieneLetraMayuscula && tieneLetraMinuscula && tieneCaracterEspecial;
+    }
+
+    tieneMinimo(password: string) {
+        return password?.length >= 8;
+    }
+
+    tieneMayuscula(password: string) {
+        return /[A-Z]/.test(password || '');
+    }
+
+    tieneMinuscula(password: string) {
+        return /[a-z]/.test(password || '');
+    }
+
+    tieneNumero(password: string) {
+        return /[0-9]/.test(password || '');
+    }
+
+    tieneSimbolo(password: string) {
+        return /[!@#$%^&*(),.?":{}|<>]/.test(password || '');
     }
 }
