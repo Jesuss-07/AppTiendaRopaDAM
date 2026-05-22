9a0d1f06866bfbd8cdd9f35fd3570db6a2554b18
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-22 19:31:51 +0200

    Se añadio la Url de home

--- Frontend/apptienda/src/app/core/config/environment.ts
+++ Frontend/apptienda/src/app/core/config/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     indexUrl: 'https://apptiendaropadam-production.up.railway.app',
     authUrl: 'https://apptiendaropadam-production.up.railway.app/auth',
-    empresaUrl: 'https://apptiendaropadam-production.up.railway.app/empresa'
+    empresaUrl: 'https://apptiendaropadam-production.up.railway.app/empresa',
+    homeUrl: 'https://apptiendaropadam-production.up.railway.app/home'
 };
