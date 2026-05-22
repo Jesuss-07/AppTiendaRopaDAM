9da333e8464f55e03c045d269609c4a7c8de9a84
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-22 23:09:51 +0200

    Se modifico ts, html y css del login ahora mostrara errores

--- Frontend/apptienda/src/app/pages/login/login.component.ts
+++ Frontend/apptienda/src/app/pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from "@angular/core";
 import { AuthService } from "../../services/auth.service";
+import { UsuarioService } from "../../services/usuario.service";
 import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
 import { RouterLink } from "@angular/router";
 import { jwtDecode } from "jwt-decode";
+import { CommonModule } from "@angular/common";
 
 
 
@@ -14,7 +16,8 @@ import { jwtDecode } from "jwt-decode";
   standalone: true,
   imports: [
     FormsModule, 
-    RouterLink
+    RouterLink,
+    CommonModule
   ]
 
 })
@@ -22,27 +25,30 @@ export class LoginComponent {
 
   email: string = "";
   password: string = "";
+  contadorErrores: number = 0;
+  emailActual: string = "";
+  mensaje: string = "";
 
 
   constructor(
     private authService: AuthService,
+    private usuarioService: UsuarioService,
     private router: Router
   ) {}
 
   onLogin() {
+    this.mensaje = "";
 
     const usuario = {
       email: this.email,
       password: this.password
     };
-            console.log("Usuario:", usuario);
-
 
     this.authService.login(usuario).subscribe({
       
       next: (res: any) => {
 
-        console.log("Login correcto", res);
+        this.mensaje = "";
 
         this.authService.guardarToken(res.token);
 
@@ -53,9 +59,30 @@ export class LoginComponent {
       },
 
       error: (error) => {
-        console.log("Usuario:", this.email);
-        console.log("Password:", this.password);
-        console.log("Error en login", error);
+
+        if (this.emailActual !== this.email) {
+          this.emailActual = this.email;
+          this.contadorErrores = 0; // Se reiniciara el contador cada vez que se cambie de email
+        } 
+
+        this.contadorErrores++;
+        this.mensaje = "Credenciales incorrectas";
+        console.log("Intentos fallidos: ", this.contadorErrores);
+            console.log("Mensaje: " + this.mensaje);
+
+        if (this.contadorErrores === 3) {
+          this.usuarioService.bloquearUsuario(this.email).subscribe({
+          next: (res: any) => {
+            console.log("Usuario bloqueado", res.mensaje);
+            this.mensaje = res.mensaje;
+          },
+          error: (err) => {
+            console.error("Error al bloquear usuario", err);
+            this.mensaje = "Error al bloquear usuario. Por favor, intente nuevamente.";
+          }
+        });
+          return;
+        }
       }
     });
   }
