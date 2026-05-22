40803baa8888ef8a676319a5530ffe9da8b45b58
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-22 20:24:32 +0200

    Se permite url user mas **

--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/security/SecurityConfig.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/security/SecurityConfig.java
@@ -69,6 +69,7 @@ public class SecurityConfig {
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/empresa/**").permitAll()
                 .requestMatchers("/home/**").permitAll()
+                .requestMatchers("/user/**").permitAll()
                 .anyRequest().authenticated())
         		.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         		.authenticationProvider(authenticationProvider)
