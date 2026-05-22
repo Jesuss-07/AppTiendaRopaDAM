668b8dec3cfd2d56a367e99629c7b0090e17fca8
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-22 23:09:10 +0200

    Se agrego nueva ruta

--- Frontend/apptienda/src/app/core/config/environment.ts
+++ Frontend/apptienda/src/app/core/config/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
     indexUrl: 'https://apptiendaropadam-production.up.railway.app',
     authUrl: 'https://apptiendaropadam-production.up.railway.app/auth',
     empresaUrl: 'https://apptiendaropadam-production.up.railway.app/empresa',
-    homeUrl: 'https://apptiendaropadam-production.up.railway.app/home'
+    homeUrl: 'https://apptiendaropadam-production.up.railway.app/home',
+    userUrl: 'https://apptiendaropadam-production.up.railway.app/user'
 };
