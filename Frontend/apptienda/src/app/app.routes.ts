bdc15d5e2268957fe2ee40ecbc2de4155c4b8a29
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-23 20:22:58 +0200

    Nuevas rutas para editar cliente y veendedor

--- Frontend/apptienda/src/app/app.routes.ts
+++ Frontend/apptienda/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
 import { EleccionComponent } from './pages/registrar/eleccion/eleccion.component';
 import { ClienteComponent } from './pages/registrar/cliente/cliente.component';
+import { EditorClienteComponent } from './pages/usuario/editor-cliente/editor.component';
 import { EmpresaComponent } from './pages/registrar/empresa/empresa.component';
 import { VendedorComponent } from './pages/registrar/vendedor/vendedor.component';
+import { EditorVendedorComponent } from './pages/usuario/editor-vendedor/editor.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
@@ -13,5 +15,7 @@ export const routes: Routes = [
     { path: 'registro/vendedor', component: VendedorComponent },
     { path: 'registro/empresa', component: EmpresaComponent },
     { path: 'inicio', component: InicioComponent },
+    { path: 'editor/cliente', component: EditorClienteComponent },
+    { path: 'editor/vendedor', component: EditorVendedorComponent },
     { path: '', redirectTo: 'login' , pathMatch: 'full' }
 ];
