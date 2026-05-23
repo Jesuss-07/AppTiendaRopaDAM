28e96bec0e8ca18b531906cbc313b29620306a8b
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-23 20:22:36 +0200

    Se configuro para poder implementar el interceptor

--- Frontend/apptienda/src/app/app.config.ts
+++ Frontend/apptienda/src/app/app.config.ts
@@ -1,11 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
+import { provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+import { authInterceptor } from './core/auth.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
