79e5968c225b5325023c72b923458aa8184fd3a9
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-23 20:23:29 +0200

    Se agregaron dos nuevos componentes para editar usuario

---
+++ Frontend/apptienda/src/app/pages/usuario/editor-cliente/editor.component.ts
@@ -0,0 +1,19 @@
+import { UsuarioService } from "../../../services/usuario.service";
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-editor-vendedor",
+  templateUrl: "./editor.component.html",
+    styleUrls: ["./editor.component.css"],
+    standalone: true,
+    imports: [
+      FormsModule,
+      CommonModule
+    ]
+})
+export class EditorClienteComponent {
+
+}
