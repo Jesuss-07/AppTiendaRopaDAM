439e1fb269e258b8090b78a03d0f2da039418502
Jesús León Díaz <jesusleondiaz02@gmail.com>
2026-05-25 04:03:57 +0200

    Se agrego un nuevo componente y se modifico el EmpresaComponent a RegistroEmpresaComponent

--- Frontend/apptienda/src/app/app.routes.ts
+++ Frontend/apptienda/src/app/app.routes.ts
@@ -4,18 +4,20 @@ import { LoginComponent } from './pages/login/login.component';
 import { EleccionComponent } from './pages/registrar/eleccion/eleccion.component';
 import { ClienteComponent } from './pages/registrar/cliente/cliente.component';
 import { EditorClienteComponent } from './pages/usuario/editor-cliente/editor.component';
-import { EmpresaComponent } from './pages/registrar/empresa/empresa.component';
+import { RegistroEmpresaComponent } from './pages/registrar/empresa/empresa.component';
 import { VendedorComponent } from './pages/registrar/vendedor/vendedor.component';
 import { EditorVendedorComponent } from './pages/usuario/editor-vendedor/editor.component';
+import { EmpresaComponent } from './pages/empresa/empresa.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'registro', component: EleccionComponent },
     { path: 'registro/cliente', component: ClienteComponent },
     { path: 'registro/vendedor', component: VendedorComponent },
-    { path: 'registro/empresa', component: EmpresaComponent },
+    { path: 'registro/empresa', component: RegistroEmpresaComponent },
     { path: 'inicio', component: InicioComponent },
     { path: 'editor/cliente', component: EditorClienteComponent },
     { path: 'editor/vendedor', component: EditorVendedorComponent },
+    { path: 'empresa', component: EmpresaComponent },
     { path: '', redirectTo: 'login' , pathMatch: 'full' }
 ];
