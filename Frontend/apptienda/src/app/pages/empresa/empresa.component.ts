0125e1bf1687b544b997d2fd805f779fb8493511
Jesús León Díaz <jesusleondiaz02@gmail.com>
2026-05-25 21:14:49 +0200

    Componente ya finalizado sin botones para crear editar o borrar empresas aun

--- Frontend/apptienda/src/app/pages/empresa/empresa.component.ts
+++ Frontend/apptienda/src/app/pages/empresa/empresa.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
+import { EmpresaService } from '../../services/empresa.service';
+import { EmpresaDTO } from '../../model/empresa.dto';
 
 @Component({
   selector: 'app-empresa',
@@ -10,19 +11,35 @@ import { AuthService } from '../../services/auth.service';
     styleUrls: ['./empresa.component.css'],
     standalone: true,
     imports: [
-        RouterLink,
         CommonModule
     ]
 })
 
 export class EmpresaComponent {
 
+  empresas: EmpresaDTO[] = [];
   rol: string | null = null;
+  cargando = true;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService, private empresaService: EmpresaService) {}
 
   ngOnInit() {
     this.rol = this.authService.getRol();
+    this.cargarEmpresas();
+  }
+
+  cargarEmpresas() {
+    this.empresaService.obtenerEmpresas().subscribe({
+      next: (data) => {
+        this.empresas = data;
+        this.cargando = false;
+        console.log("✅ RESPONSE OBJETO:", this.empresas);
+    },
+      error: (err) => {
+        console.error('Error al cargar las empresas:', err);
+        this.cargando = false;
+      }
+    });
   }
 
   logout() {
