c73a56a66b81fa749e9d0fdf11ea0c21d4fc27f3
Jesús León Díaz <jesusleondiaz02@gmail.com>
2026-05-25 21:45:42 +0200

    Se corrigio el fallo de carga de las empresas

--- Frontend/apptienda/src/app/pages/empresa/empresa.component.ts
+++ Frontend/apptienda/src/app/pages/empresa/empresa.component.ts
@@ -4,41 +4,29 @@ import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { EmpresaService } from '../../services/empresa.service';
 import { EmpresaDTO } from '../../model/empresa.dto';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-empresa',
   templateUrl: './empresa.component.html',
-    styleUrls: ['./empresa.component.css'],
-    standalone: true,
-    imports: [
-        CommonModule
-    ]
+  styleUrls: ['./empresa.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
-
 export class EmpresaComponent {
 
-  empresas: EmpresaDTO[] = [];
+  empresas$!: Observable<EmpresaDTO[]>;
   rol: string | null = null;
+
   cargando = true;
 
   constructor(private router: Router, private authService: AuthService, private empresaService: EmpresaService) {}
 
   ngOnInit() {
     this.rol = this.authService.getRol();
-    this.cargarEmpresas();
-  }
-
-  cargarEmpresas() {
-    this.empresaService.obtenerEmpresas().subscribe({
-      next: (data) => {
-        this.empresas = data;
-        this.cargando = false;
-        console.log("✅ RESPONSE OBJETO:", this.empresas);
-    },
-      error: (err) => {
-        console.error('Error al cargar las empresas:', err);
-        this.cargando = false;
-      }
+    this.empresas$ = this.empresaService.obtenerEmpresas();
+    this.empresas$.subscribe(() => {
+      this.cargando = false;
     });
   }
 
