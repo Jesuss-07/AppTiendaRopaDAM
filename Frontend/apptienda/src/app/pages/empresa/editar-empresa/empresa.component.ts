ddf758264259bdb89633566e21c1fc3ec4ef66b5
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-27 01:08:26 +0200

    Componentes que permiten editar una empresa

--- Frontend/apptienda/src/app/pages/empresa/editar-empresa/empresa.component.ts
+++ Frontend/apptienda/src/app/pages/empresa/editar-empresa/empresa.component.ts
@@ -3,6 +3,9 @@ import { AuthService } from '../../../services/auth.service';
 import { EmpresaService } from '../../../services/empresa.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ActualizarEmpresaDTO } from '../../../model/actualizar-empresa.dto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-empresa',
@@ -10,17 +13,45 @@ import { CommonModule } from '@angular/common';
     styleUrl: './empresa.component.css',
     standalone: true,
     imports: [
-        CommonModule
+        CommonModule,
+        FormsModule
     ]
 })
 export class EditarEmpresaComponent {
 
     rol: string | null = null;
+    idEmpresa: number = 0;
+    empresa: ActualizarEmpresaDTO = {
+        nombreEmpresa: '',
+        emailContacto: '',
+        telefonoContacto: '',
+        direccionSede: '',
+        logoEmpresa: ''
+    };
 
-    constructor(private authService: AuthService, private empresaService: EmpresaService, private router: Router) { }
+    constructor(private authService: AuthService, private empresaService: EmpresaService, private router: Router, private route: ActivatedRoute) { }
 
     ngOnInit() {
         this.rol = this.authService.getRol();
+        this.idEmpresa = Number(this.route.snapshot.paramMap.get('id'));
+        console.log('ID de la empresa:', this.idEmpresa);
+
+        this.empresaService.obtenerEmpresaPorId(this.idEmpresa).subscribe({
+            next: (data) => { this.empresa = data; },
+            error: (err) => { console.error('Error al obtener la empresa:', err); }
+        });
+
+    }
+
+    actualizarEmpresa() {
+        this.empresaService.actualizarEmpresa(this.idEmpresa, this.empresa).subscribe({
+            next: () => {
+                this.router.navigate(['/empresa']);
+            },
+            error: (err) => {
+                console.error('Error al actualizar la empresa:', err);
+            }
+        });
     }
 
     logout() {
