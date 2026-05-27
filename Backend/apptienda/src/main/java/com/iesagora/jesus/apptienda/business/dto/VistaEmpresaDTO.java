d710960abf10981cf5577d8dca969c0a4a0a4a55
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-27 09:31:46 +0200

    Se agregaron dos DTO para la pagina de Vista empresa

---
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/dto/VistaEmpresaDTO.java
@@ -0,0 +1,22 @@
+package com.iesagora.jesus.apptienda.business.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class VistaEmpresaDTO {
+	
+	private String nombreEmpresa;
+	private String cif;
+	private String emailContacto;
+	private String telefonoContacto;
+	private String direccionSede;
+	private String logoEmpresa;
+}
