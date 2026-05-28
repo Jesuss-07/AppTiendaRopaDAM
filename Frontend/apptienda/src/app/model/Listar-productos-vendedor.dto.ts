2f2ce464f848b45f5cb43081cbb04dc98be56c77
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-28 15:50:10 +0200

    Se agrego DTO para listar los productos perteneciente a cada empresa del vendedor correspondiente

---
+++ Frontend/apptienda/src/app/model/Listar-productos-vendedor.dto.ts
@@ -0,0 +1,10 @@
+export interface ListarProductosVendedorDTO {
+    idProducto: number;
+    nombreProducto: string;
+    precio: number;
+    stock: number;
+    talla: string;
+    color: string;
+    categoria: string;
+    imagenProducto: string;
+}
