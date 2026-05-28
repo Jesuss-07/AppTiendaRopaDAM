1abd2598a9d131a87f0264b9550db431e93267d9
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-28 16:32:09 +0200

    Se importo nuevo DTO para editar productos y ya esta disponible la nueva llamada al backend para editar productos

--- Frontend/apptienda/src/app/services/producto.service.ts
+++ Frontend/apptienda/src/app/services/producto.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../core/config/environment';
 import { Observable } from 'rxjs';
+import { CrearProductoDTO } from '../model/crear-producto.dto';
 import { ListarProductosVendedorDTO } from '../model/Listar-productos-vendedor.dto';
+import { EditarProductoDTO } from '../model/editar-producto.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,13 +13,12 @@ export class ProductoService {
   private apiUrl = environment.productUrl;
   constructor(private http: HttpClient) {}
 
-  anadirProducto(producto: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/crear`, producto);
+  anadirProducto(producto: CrearProductoDTO): Observable<CrearProductoDTO> {
+    return this.http.post<CrearProductoDTO>(`${this.apiUrl}/crear`, producto);
   }
 
-  //METODO NO LISTO
-  editarProducto(id: number, productoDTO: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/editar/${id}`, productoDTO);
+  editarProducto(id: number, productoDTO: EditarProductoDTO): Observable<EditarProductoDTO> {
+    return this.http.put<EditarProductoDTO>(`${this.apiUrl}/editar/${id}`, productoDTO);
   }
 
   //METODO NO LISTO
