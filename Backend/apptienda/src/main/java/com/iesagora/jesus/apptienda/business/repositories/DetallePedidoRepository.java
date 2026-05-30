a3f6fe5bc848b352eada9e6aa97983b448e55260
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-30 17:54:22 +0200

    Ahora se puede pedir una lista de detalle pedido por id

--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/repositories/DetallePedidoRepository.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/repositories/DetallePedidoRepository.java
@@ -1,5 +1,6 @@
 package com.iesagora.jesus.apptienda.business.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,7 @@ import com.iesagora.jesus.apptienda.business.model.DetallePedido;
 public interface DetallePedidoRepository extends JpaRepository<DetallePedido, Long>{
 	
 	Optional<DetallePedido> findByIdPedidoAndIdProducto(Long idPedido, Long idProducto);
+	
+	List<DetallePedido> findByIdPedido(Long idPedido);
 
 }
