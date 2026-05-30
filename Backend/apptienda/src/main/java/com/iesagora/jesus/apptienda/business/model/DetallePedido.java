6ad9b44d32983702b9000cdbfbabb5855324bd5b
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-30 16:29:14 +0200

    ERROR DE PRIMARYKEYJOINCOLUMN

--- Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/model/DetallePedido.java
+++ Backend/apptienda/src/main/java/com/iesagora/jesus/apptienda/business/model/DetallePedido.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,7 +21,6 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table(name = "DETALLE_PEDIDO")
-@PrimaryKeyJoinColumn(name = "id_detalle")
 public class DetallePedido {
 	
 	@Id
