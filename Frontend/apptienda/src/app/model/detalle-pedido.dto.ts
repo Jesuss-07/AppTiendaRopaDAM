e01baf6c4245d41602262e490d2cde9e6a1fbd4b
Jesús León Díaz <jesusleondiaz07@gmail.com>
2026-05-31 13:46:40 +0200

    Se agrego el id

--- Frontend/apptienda/src/app/model/detalle-pedido.dto.ts
+++ Frontend/apptienda/src/app/model/detalle-pedido.dto.ts
@@ -1,6 +1,7 @@
 import { ProductoCarritoDTO } from "./producto-carrito.dto";
 
 export interface DetallePedidoDTO {
+    idDetallePedidoDTO: number;
     productoCarritoDTO: ProductoCarritoDTO;
     cantidad: number;
     precioUnidad: number;
